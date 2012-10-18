35a0b2aadaee2d41afadcf23848e2712695c54dd
Niklas Andren <niklas.andren@hotmail.com>
2012-10-18 16:37:07 +0200

    merged with comments-css

---
+++ Mission.WebUI/Content/js/comments.js
@@ -0,0 +1,15 @@
+﻿jQuery(document).ready(function () {
+
+    $(".comments").click(function () {
+
+        if ($(this).parent().next().hasClass('selected')) {
+            $(this).parent().next().removeClass('selected').slideToggle('fast', function () { });
+        }
+        else {
+            $(this).parent().next().addClass('selected').slideToggle('fast', function () { });
+        }
+
+
+    });
+
+});
