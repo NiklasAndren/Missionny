a53aec8170c1a09ff165e46105f0932ac58f4b61
kalle <kalle.r.persson@gmail.com>
2012-11-20 17:12:37 +0100

    jag har gjoert ngt...

---
+++ Mission.Domain/Entities/Words.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mission.Domain.Entities.Abstract;
+
+namespace Mission.Domain.Entities
+{
+    public class Words : IEntity
+    {
+        public Guid ID { get; set; }
+        public string Word { get; set; }
+        public int WordCount { get; set; }
+    }
+}
