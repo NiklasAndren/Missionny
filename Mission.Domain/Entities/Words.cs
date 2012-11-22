8dbb0be48512e9ef616174eeac3e4f1b2029d06d
kalle <kalle.r.persson@gmail.com>
2012-11-22 16:22:28 +0100

    yearstatistics helt helt klar

--- Mission.Domain/Entities/Words.cs
+++ Mission.Domain/Entities/Words.cs
@@ -11,5 +11,6 @@ namespace Mission.Domain.Entities
         public Guid ID { get; set; }
         public string Word { get; set; }
         public int WordCount { get; set; }
+    //    public DateTime Date { get; set; }
     }
 }
